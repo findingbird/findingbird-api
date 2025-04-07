7b2b365f938ce0564a08cdf6974f1bb7d83b3122
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-07 21:26:51 +0900

    feat: 전역 설정 추가
    - 전역 에러필터 추가
    - 전역 로깅 인터셉터 추가
    - 전역 validation pipe 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import '~/config/dayjs.config';
 
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '~/app.controller';
 import { AppService } from '~/app.service';
+import { GlobalExceptionFilter } from '~/common/filters/Global-exception.filter';
+import { LoggingInterceptor } from '~/common/interceptors/Logging.interceptor';
 import { TypeOrmConfig } from '~/config/typeorm.config';
 
 @Module({
@@ -19,6 +22,29 @@ import { TypeOrmConfig } from '~/config/typeorm.config';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        forbidUnknownValues: true,
+        stopAtFirstError: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/common/filters/Global-exception.filter.ts
@@ -0,0 +1,24 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+import { DateUtils } from '~/common/utils/Date.utils';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message = exception instanceof HttpException ? exception.getResponse() : 'Internal server error';
+
+    response.status(status).json({
+      success: false,
+      timestamp: DateUtils.format(DateUtils.now()),
+      path: request.url,
+      message: typeof message === 'object' ? message : { error: message },
+    });
+  }
+}
