e5be4076c193129b47bb26160501d8fd01ae85bf
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-07 20:28:10 +0900

    feat: 포트설정 환경변수 사용

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '~/app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  const port = parseInt(config.get<string>('PORT') ?? '3000');
   app.enableCors();
-
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
