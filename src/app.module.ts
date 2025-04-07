a4b0752b11ab29b063056f3f3ee7745bc96966b7
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-07 20:28:57 +0900

    feat: DateUtil 추가
    - 코드레벨에선 KST로 시간 관리
    - DB에 저장할 때 UTC 형태로 저장
    - 조회시 다시 KST로 변환하여 조회

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import '~/config/dayjs.config';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
---
+++ src/common/utils/Date.utils.ts
@@ -0,0 +1,23 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+export class DateUtils {
+  // 현재 시간 (KST) 기준
+  static now(): Dayjs {
+    return dayjs().tz();
+  }
+
+  // 어떤 날짜든 KST로 변환
+  static toKst(d: Dayjs | Date | string): Dayjs {
+    return dayjs(d).tz();
+  }
+
+  // KST로 포맷
+  static format(d: Dayjs | Date | string, format: string = 'YYYY-MM-DD HH:mm:ss'): string {
+    return this.toKst(d).format(format);
+  }
+
+  // UTC 저장용 Date 객체로 변환
+  static toUtcDate(d: Dayjs | Date | string): Date {
+    return this.toKst(d).utc().toDate();
+  }
+}
