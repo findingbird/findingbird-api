7b214e5202b59ea6c1cf8c76aef7f23b24926812
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-12 23:14:32 +0900

    refactor: core entity ID 컬럼 수정
    - 도메인 레이어에서 uuid 생성해서 할당

--- src/common/entities/Core.entity.ts
+++ src/common/entities/Core.entity.ts
@@ -1,15 +1,32 @@
-import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { CreateDateColumn, DeleteDateColumn, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class CoreEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn({
+    type: 'varchar',
+    name: 'id',
+    comment: 'ID',
+  })
+  id: string;
 
-  @CreateDateColumn({ type: 'timestamp with time zone' })
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+    name: 'created_at',
+    comment: '생성일시 (UTC)',
+  })
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+    name: 'updated_at',
+    comment: '수정일시 (UTC)',
+  })
   updatedAt: Date;
 
-  @DeleteDateColumn({ type: 'timestamp with time zone', nullable: true })
+  @DeleteDateColumn({
+    type: 'timestamp with time zone',
+    name: 'deleted_at',
+    comment: '삭제일시 (UTC)',
+    nullable: true,
+  })
   deletedAt: Date | null;
 }
