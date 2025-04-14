88c472d4ccab12dce29102cef44a6778741bbce4
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-14 17:01:13 +0900

    mod: 토큰 반환 방식 변경
    - access token 은 response 로 반환
    - 추후 인증 시 bearer 헤더 사용
    - refresh token 은 http only cookie 로 반환
    - 추후 refresh 시 cookie 에서 추출하여 사용

--- src/modules/auth/presentation/http/auth.controller.ts
+++ src/modules/auth/presentation/http/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
 
 import { JwtRefreshGuard } from '~/common/guards/jwt-refresh-guard';
 import { KakaoRequest } from '~/common/interfaces/kakao-request.interface';
 import { UserRequest } from '~/common/interfaces/user-request.interface';
 import { KakaoLoginDto } from '~/modules/auth/application/dtos/kakao-login.dto';
-import { TokenDto } from '~/modules/auth/application/dtos/token.dto';
+import { AccessTokenDto } from '~/modules/auth/application/dtos/token.dto';
 import { AuthService } from '~/modules/auth/application/services/auth.service';
 
 @Controller('auth')
@@ -20,20 +21,33 @@ export class AuthController {
 
   @Get('/kakao/redirect')
   @UseGuards(AuthGuard('kakao'))
-  async kakaoLoginRedirect(@Req() req: KakaoRequest): Promise<TokenDto> {
+  async kakaoLoginRedirect(
+    @Req() req: KakaoRequest,
+    @Res({ passthrough: true }) res: Response
+  ): Promise<AccessTokenDto> {
     const kakaoLoginDto: KakaoLoginDto = req.user;
     const token = await this.authService.kakaoLogin(kakaoLoginDto);
-    return token;
+    res.cookie('refresh-token', token.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: this.authService.refreshExpiresInSecond * 1000,
+    });
+    return { accessToken: token.accessToken };
   }
 
   @Post('/refresh')
   @UseGuards(JwtRefreshGuard)
-  async refreshToken(@Req() req: UserRequest): Promise<TokenDto> {
+  async refreshToken(@Req() req: UserRequest, @Res({ passthrough: true }) res: Response): Promise<AccessTokenDto> {
     const refreshToken = req.cookies['refresh-token'];
     const userId = req.user.userId;
     const token = await this.authService.refreshToken(userId, refreshToken);
-    return token;
+    res.cookie('refresh-token', token.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      maxAge: this.authService.refreshExpiresInSecond * 1000,
+    });
+    return { accessToken: token.accessToken };
   }
 }
-
-// TODO: 토큰 쿠키에 담기
