9d126f2530e75b56c3323dd794258be7f5a796de
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-14 17:26:54 +0900

    feat: swagger 문서 추가

--- src/modules/user/presentation/http/user.controller.ts
+++ src/modules/user/presentation/http/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '~/common/guards/jwt-auth-guard';
 import { UserRequest } from '~/common/interfaces/user-request.interface';
+import { DateUtils } from '~/common/utils/Date.utils';
 import { UserService } from '~/modules/user/application/services/user.service';
-import { User } from '~/modules/user/domain/models/user';
+import { UserResponseDto } from '~/modules/user/presentation/http/dtos/user.response';
 
 @Controller('user')
 export class UserController {
@@ -11,8 +13,20 @@ export class UserController {
 
   @Get('/')
   @UseGuards(JwtAuthGuard)
-  async getUserById(@Req() req: UserRequest): Promise<User> {
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '사용자 정보 조회' })
+  @ApiResponse({
+    status: 200,
+    description: '사용자 정보 조회 성공',
+    type: UserResponseDto,
+  })
+  async getUserById(@Req() req: UserRequest): Promise<UserResponseDto> {
     const userId = req.user.userId;
-    return await this.userService.getUserById({ userId });
+    const user = await this.userService.getUserById({ userId });
+    return {
+      id: user.id,
+      nickname: user.nickname,
+      createdAt: DateUtils.format(user.createdAt),
+    };
   }
 }
