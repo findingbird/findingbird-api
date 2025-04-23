565bc8d21d5157060366fc0f8e57fd1a429892e0
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-23 21:02:39 +0900

    refactor: access token URL query 에 담아 redirect 해주는 방식으로 수정

--- src/modules/auth/presentation/http/auth.controller.ts
+++ src/modules/auth/presentation/http/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Response } from 'express';
@@ -12,7 +13,10 @@ import { TokenResponseDto } from '~/modules/auth/presentation/http/dtos/token.re
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('/kakao')
   @UseGuards(AuthGuard('kakao'))
@@ -21,10 +25,8 @@ export class AuthController {
     description: '자동으로 카카오 로그인 페이지로 redirect 됩니다. (Swagger 상에서는 동작하지 않습니다.)',
   })
   @ApiResponse({
-    status: 200,
-    description:
-      'access token은 response body, refresh token은 refresh-token cookie에 포함되어 있습니다. (https 가 활성화되어야 합니다.)',
-    type: TokenResponseDto,
+    status: 302,
+    description: '카카오 로그인 페이지로 redirect 됩니다.',
   })
   async kakaoLogin(): Promise<void> {
     // redirect to kakao login page
@@ -34,12 +36,15 @@ export class AuthController {
   @UseGuards(AuthGuard('kakao'))
   @ApiOperation({
     summary: '카카오 로그인 후 자동 redirect 되는 url',
-    description: '카카오 서버에서 응답을 보내는 엔드포인트 입니다. 클라이언트 단에서 요청하지 않습니다.',
+    description:
+      '카카오 서버에서 응답을 보내는 엔드포인트 입니다. 클라이언트 단에서 요청하지 않습니다. 클라이언트의 callback url로 redirect 됩니다',
   })
-  async kakaoLoginRedirect(
-    @Req() req: KakaoRequest,
-    @Res({ passthrough: true }) res: Response
-  ): Promise<TokenResponseDto> {
+  @ApiResponse({
+    status: 302,
+    description:
+      'access token은 URL query, refresh token은 cookie에 포함되어 있습니다. (https 가 활성화되어야 합니다.)',
+  })
+  async kakaoLoginRedirect(@Req() req: KakaoRequest, @Res() res: Response): Promise<void> {
     const kakaoLoginDto: KakaoLoginDto = req.user;
     const token = await this.authService.kakaoLogin(kakaoLoginDto);
     res.cookie('refresh-token', token.refreshToken, {
@@ -48,7 +53,10 @@ export class AuthController {
       sameSite: 'none',
       maxAge: this.authService.refreshExpiresInSecond * 1000,
     });
-    return { accessToken: token.accessToken };
+    const clientUrl = this.configService.get<string>('CLIENT_URL');
+    // TODO: client redirect URL 확인 및 수정"
+    const redirectUrl = `${clientUrl}/auth/callback?accessToken=${token.accessToken}`;
+    return res.redirect(redirectUrl);
   }
 
   @Post('/refresh')
@@ -60,7 +68,7 @@ export class AuthController {
   @ApiResponse({
     status: 200,
     description:
-      'access token은 response body, refresh token은 refresh-token cookie에 포함되어 있습니다. (https 가 활성화되어야 합니다.)',
+      'access token은 response body, refresh token은 cookie에 포함되어 있습니다. (https 가 활성화되어야 합니다.)',
     type: TokenResponseDto,
   })
   async refreshToken(@Req() req: UserRequest, @Res({ passthrough: true }) res: Response): Promise<TokenResponseDto> {
