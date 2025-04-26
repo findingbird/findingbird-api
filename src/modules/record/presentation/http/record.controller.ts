19d273cf51e39abf7c64cef9075bbb61afb46030
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-26 13:43:25 +0900

    feat: 관찰기록 한 달 단위 조회 기능 추가

--- src/modules/record/presentation/http/record.controller.ts
+++ src/modules/record/presentation/http/record.controller.ts
@@ -1,19 +1,79 @@
-import { Body, Controller, Get, Param, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '~/common/guards/jwt-auth-guard';
 import { UserRequest } from '~/common/interfaces/user-request.interface';
+import { DateUtils } from '~/common/utils/Date.utils';
 import { RecordService } from '~/modules/record/application/services/record.service';
 import {
   CreateRecordRequestDto,
   CreateRecordSwaggerDto,
 } from '~/modules/record/presentation/http/dtos/create-record.dto';
 import { RecordResponseDto } from '~/modules/record/presentation/http/dtos/record.reseponse.dto';
+import {
+  RecordByMonthResponseDto,
+  RecordMonthQueryDto,
+  RecordPreviewDto,
+} from '~/modules/record/presentation/http/dtos/record-by-month.dto';
 
 @Controller('record')
 export class RecordController {
   constructor(private readonly recordService: RecordService) {}
+
+  @Get('/')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: '기록 월단위 조회',
+    description:
+      '해당 월의 기록을 조회합니다. 모든 날짜에 대한 응답이 담겨있으며 해당 날짜에 기록이 없을 경우 빈 배열이 반환됩니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '기록 월단위 조회 성공',
+    type: RecordByMonthResponseDto,
+  })
+  async getRecordsByMonth(
+    @Req() req: UserRequest,
+    @Query() query: RecordMonthQueryDto
+  ): Promise<RecordByMonthResponseDto> {
+    const { userId } = req.user;
+    const { year, month } = query;
+    const records = await this.recordService.getRecordsByMonth({ userId, year, month });
+
+    const lastDayOfMonth = new Date(year, month, 0).getDate();
+    const recordsByDate = new Map<number, RecordPreviewDto[]>();
+    for (let day = 1; day <= lastDayOfMonth; day++) {
+      recordsByDate.set(day, []);
+    }
+
+    for (const record of records) {
+      const date = record.createdAt.date();
+      recordsByDate.get(date)?.push({ id: record.id, createdAt: DateUtils.format(record.createdAt) });
+    }
+
+    const dailyRecords = Array.from(recordsByDate.entries()).map(([date, records]) => ({
+      date,
+      records,
+      hasRecords: records.length > 0,
+    }));
+
+    dailyRecords.sort((a, b) => a.date - b.date);
+
+    return { dailyRecords };
+  }
 
   @Get('/:id')
   @UseGuards(JwtAuthGuard)
