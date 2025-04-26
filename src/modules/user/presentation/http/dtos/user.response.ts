db06518de03cd617f724cf10ee6d25ed8aac7dcd
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-04-26 14:36:06 +0900

    docs: swagger example 명시

--- src/modules/user/presentation/http/dtos/user.response.ts
+++ src/modules/user/presentation/http/dtos/user.response.ts
@@ -1,12 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserResponseDto {
-  @ApiProperty({ description: 'user 고유 Id' })
+  @ApiProperty({
+    description: 'user 고유 Id',
+    example: '4dcc664b-93f2-4179-9ff1-d838164feb54',
+  })
   id: string;
 
-  @ApiProperty({ description: 'user nickname(랜덤 생성)' })
+  @ApiProperty({
+    description: 'user nickname(랜덤 생성)',
+    example: 'user5394',
+  })
   nickname: string;
 
-  @ApiProperty({ description: '가입 일자' })
+  @ApiProperty({
+    description: '가입 일자',
+    example: '2025-04-14 10:13:41',
+  })
   createdAt: string;
 }
